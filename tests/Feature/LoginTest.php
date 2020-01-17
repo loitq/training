ac47a277c97fb38057afcb51937c7d650c1cf211
loitq <loitq@lifull-tech.vn>
2020-01-17 15:45:33 +0700

    demo testing

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -5,16 +5,30 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\User;
+use Hash;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class LoginTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
-    * A basic feature test user view login.
+    * test url is not login.
     **/
-    public function testViewLogin()
+    public function testUrlIsNotLogin()
     {
-        $response = $this->get('/login');
-        $response->assertViewIs('auth.login');
+        $response = $this->get('/abc');
+        $response->assertSee('404');
+    }
+
+    /**
+    * Not login go to path admin.
+    **/
+    public function testFirstPathAdmin()
+    {
+        $response = $this->get('/admin');
+        $response->assertRedirect('login');
     }
 
     /**
@@ -24,5 +38,39 @@ class LoginTest extends TestCase
     {
         $response = $this->get('/login');
         $response->assertViewIs('auth.login');
+    }
+
+    // /**
+    // * Feature test user wrong
+    // **/
+    // public function testUserWrong()
+    // {
+    //     $response = $this->from('/login')->post('/login', [
+    //         'email' => 'user@lifull-tech.vn',
+    //         'password' => 'invalid-password'
+    //     ]);
+    //     $response->assertViewIs('auth.login');
+    //     $response->assertSee('These credentials do not match our records.');
+    //     $response->assertSessionHasErrors('email');
+    //     $this->assertTrue(session()->hasOldInput('email'));
+    //     $this->assertFalse(session()->hasOldInput('password'));
+    //     $this->assertGuest();
+    // }
+
+    /**
+    * Test user login.
+    */
+    public function testUserLogin()
+    {
+        $user = User::find(1);
+
+        $response = $this->post('/login', [
+            'email' => 'user@lifull-tech.vn',
+            'password' => Hash::make('lifull@123'),
+            '_token' => csrf_token()
+        ]);
+
+        $response->assertRedirect('/');
+        $this->assertAuthenticatedAs($user);
     }
 }
