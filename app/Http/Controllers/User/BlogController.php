a7a9861807219ef9d6299ea18ace4cbb6e2b451c
thinhnt <thinhnt@lifull-tech.vn>
2020-01-22 16:12:31 +0700

    Fix permission

--- app/Http/Controllers/User/BlogController.php
+++ app/Http/Controllers/User/BlogController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Blog;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class BlogController extends Controller
 {
+
+    // return information user
+    public function defineUser() {
+        return Auth::user();
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -17,7 +24,12 @@ class BlogController extends Controller
     public function index()
     {
         $blogs = Blog::all();
-        return view('blog/index', ['blogs'=>$blogs]);
+        $canDelete = $this->defineUser()->can_delete;
+        $canSee = $this->defineUser()->can_see;
+        if($canSee == true)
+            return view('blog/index', ['blogs'=>$blogs, 'canDelete'=>$canDelete, 'canSee'=>$canSee]);
+        else
+            return redirect()->back();
     }
 
     /**
@@ -33,7 +45,7 @@ class BlogController extends Controller
             'content' => 'required',
         ]);
         $blog = new Blog;
-        $blog->user_id = Auth::user()->id;
+        $blog->user_id = $this->defineUser()->id;
         $blog->title = $request->title;
         $blog->content = $request->content;
         $blog->save();
