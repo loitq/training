36bbfa2d44dcc51d2c3efd8e8192989088fbfaad
loitq <loitq@lifull-tech.vn>
2020-02-05 10:39:28 +0700

    fix comment, use contsant

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\User;
+use Hash;
+
+class LoginTest extends DuskTestCase
+{
+    const PATH_ADMIN = '/admin';
+    const PATH_LOGIN = '/login';
+
+    /**
+     * A basic browser url not login.
+     */
+    public function testPathWrong()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/abc')
+                    ->assertSee('404');
+        });
+    }
+
+    /**
+     * A basic browser go to path admin dislay Login.
+     */
+    public function testPathAdmin()
+    {   
+        $this->browse(function (Browser $browser) {
+            $browser->visit(self::PATH_ADMIN)
+                    ->assertPathIs(self::PATH_LOGIN);
+        });
+    }
+
+    /**
+     * Display login test register link.
+     */
+    public function testRegisterLink()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(self::PATH_ADMIN)
+                    ->clickLink('Register')
+                    ->assertPathIs('/register');
+        });
+    }
+
+    /**
+     * Display login test forget password link.
+     */
+    public function testForgetPasswordLink()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(self::PATH_LOGIN)
+                    ->clickLink('Forgot Your Password?')
+                    ->assertPathIs('/password/reset');
+        });
+    }
+
+    /**
+     * A basic browser dislay Login.
+     */
+    public function testLinkForgetPassword()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(self::PATH_LOGIN)
+                    ->clickLink('Forgot Your Password?')
+                    ->assertPathIs('/password/reset');
+        });
+    }
+
+    /**
+     * A basic browser go to Forget passord.
+     */
+    public function testDisplayLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(self::PATH_LOGIN)
+                    ->assertSee('Login');
+        });
+    }
+
+    /**
+     * Test email or password wrong input keep email.
+     */
+    public function testUserWrong()
+    {
+        // Get user
+        $user = User::find(2);
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit(self::PATH_LOGIN)
+                    ->type('email', $user->email)
+                    ->type('password', 'abc@123')
+                    ->press('Login')
+                    ->assertSee('These credentials do not match our records.')
+                    ->assertInputValue('email', $user->email);
+        });
+    }
+
+    /**
+     * Test login not input.
+     */
+    public function testLoginNotInput()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit(self::PATH_LOGIN)
+                    ->press('Login')
+                    ->assertFocused('email');
+        });
+    }
+
+    /**
+     * Test email invalid
+     */
+    public function testEmailInputInvalid()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit(self::PATH_LOGIN)
+                    ->type('email', 'abc')
+                    ->press('Login')
+                    ->assertFocused('email');
+        });
+    }
+
+    /**
+     * A basic browser test user login.
+     */
+    public function testUserLogin()
+    {
+        // Get user
+        $user = User::find(2);
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit(self::PATH_LOGIN)
+                    ->type('email', $user->email)
+                    ->type('password', 'lifull@123')
+                    ->press('Login')
+                    ->assertPathIs('/');
+        });
+    }
+
+    /**
+     * A basic browser test user visit path admin.
+     */
+    public function testUserVisitAdmin()
+    {
+        $this->browse(function ($browser) {
+            $browser->loginAs(User::find(2))
+                    ->visit(self::PATH_ADMIN)
+                    ->assertPathIs('/');
+        });
+    }
+
+    /**
+     * A basic browser test user logout.
+     */
+    public function testUserLogout()
+    {
+        $this->browse(function ($browser) {
+            $browser->loginAs(User::find(2))
+                    ->logout()
+                    ->assertGuest();
+        });
+    }
+
+    /**
+     * A basic browser test admin login.
+     */
+    public function testAdminLogin()
+    {
+        // Get Admin
+        $user = User::find(1);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit(self::PATH_LOGIN)
+                    ->type('email', $user->email)
+                    ->type('password', 'lifull@123')
+                    ->press('Login')
+                    ->assertPathIs(self::PATH_ADMIN);
+        });
+    }
+
+    /**
+     * A basic browser test admmin visit path /.
+     */
+    public function testPathVisitUser()
+    {
+        $this->browse(function ($browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/')
+                    ->assertPathIs(self::PATH_ADMIN);
+        });
+    }
+
+    /**
+     * Test admin logout.
+     */
+    public function testAdminLogout()
+    {
+        $this->browse(function ($browser) {
+            $browser->loginAs(User::find(1))
+                    ->logout()
+                    ->assertGuest();
+        });
+    }
+
+    /**
+     * A basic browser user login, remove database, reload page.
+     */
+    public function testUserLoginAfterRemoveData()
+    {
+        // Create data test
+        $userDestroy = User::create([
+            'name'     => "user destroy",
+            'email'    => "usertest@lifull-tech.vn",
+            'role'     => \App\User::USER,
+            'password' => Hash::make("lifull@123")
+        ]);
+        // Login by data user test
+        $this->browse(function ($browser) use ($userDestroy) {
+            $browser->loginAs($userDestroy)
+                    ->visit('/')
+                    ->assertPathIs('/');
+        });
+        // Destroy user
+        User::destroy($userDestroy->id);
+        // Redirect path
+        $this->browse(function ($browser) {
+            $browser->visit('/')
+                    ->assertPathIs(self::PATH_LOGIN);
+        });
+    }
+}
