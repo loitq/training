c5b417fb4c430fd41b29205a0110c40a377493d3
Nguyễn Tiến Thịnh <59555237+nguyentienthinh@users.noreply.github.com>
2020-02-10 11:37:30 +0700

    Revert "Merge develop into blog"

--- resources/views/admin/user/createOrUpdate.blade.php
+++ resources/views/admin/user/createOrUpdate.blade.php
@@ -11,62 +11,37 @@
         </div>
         <!-- /.row -->
         <div class="row">
-            <div class="panel-body">
-                <a href="/admin/user/list" class="btn btn-primary">List user</a>
-            </div>
             <div class="col-lg-12">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        @if(count($errors) > 0)
-                            <div class="alert alert-danger">
-                                @foreach($errors->all() as $err)
-                                    <strong>{{$err}}</strong><br>
-                                @endforeach
-                            </div>
-                        @endif
-                        @if(session('error'))
-                            <div class="alert alert-danger">
-                                <strong>{{session('error')}}</strong>
-                            </div>
-                        @endif
-                        @if (session('message'))
-                        <div class="alert alert-info alert-dismissable">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <p>{{ session('message') }}</p>
-                        </div>
-                        @endif
                         <div class="row">
                             <div class="col-lg-6">
-                                @if (isset($edit))
-                                    <form action="/admin/user/edit/{{ $edit->id }}" method="POST" role="form">
-                                @else
-                                    <form action="/admin/user/create" method="POST" role="form">
-                                @endif
-                                    {{ csrf_field() }}
+                                <form role="form">
                                     <div class="form-group">
                                         <label>Username</label>
-                                        <input class="form-control" name="username" @if (isset($edit)) disabled value="{{ $edit->name }}" @endif>
+                                        <input class="form-control">
                                     </div>
                                     <div class="form-group">
                                         <label>Email</label>
-                                        <input class="form-control" type="email" name="email" @if (isset($edit)) disabled value="{{ $edit->email }}" @endif>
+                                        <input class="form-control" type="email">
                                     </div>
-                                    @if (!isset($edit))
-                                        <div class="form-group">
-                                            <label>Password</label>
-                                            <input class="form-control" type="password" name="password">
-                                        </div>
-                                    @endif
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input class="form-control" type="password">
+                                    </div>
                                     <div class="form-group">
                                         <label>Only see blog</label>
-                                        <input class="form-control" type="checkbox" name="can_see" @if (isset($edit)) @if (!empty($edit->can_see)) checked="checked" @endif @endif>
+                                        <input class="form-control" type="checkbox">
                                     </div>
                                     <div class="form-group">
                                         <label>Can delete blog</label>
-                                        <input class="form-control" type="checkbox" name="can_delete" @if (isset($edit)) @if (!empty($edit->can_delete)) checked="checked" @endif @endif>
+                                        <input class="form-control" type="checkbox">
                                     </div>
                                     <button type="submit" class="btn btn-default">
                                         Submit
+                                    </button>
+                                    <button type="reset" class="btn btn-default">
+                                        Reset
                                     </button>
                                 </form>
                             </div>
