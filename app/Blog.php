78ad2904504dd9c11151c25844e206dfadb98b08
thinhnt <thinhnt@lifull-tech.vn>
2020-02-10 10:53:00 +0700

    add softdeletes

--- app/Blog.php
+++ app/Blog.php
@@ -2,11 +2,23 @@
 
 namespace App;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Blog extends Model
 {
     protected $table = 'blogs';
+
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
     const IS_FALSE = 0;
     const IS_TRUE = 1;
 }
