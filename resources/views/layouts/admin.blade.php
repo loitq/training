5e2f2196b6e0f990783c5f4770f1b0654ee625bc
loitq <loitq@lifull-tech.vn>
2020-02-11 11:30:44 +0700

    Change text laravel

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Training') }}</title>
 
     <!-- Core CSS - Include with every page -->
     <link href="/asset/admin/css/bootstrap.min.css" rel="stylesheet">
