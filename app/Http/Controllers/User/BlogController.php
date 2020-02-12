ee4627e4d60f9162819af4b72513100ae5438b57
Nguyễn Tiến Thịnh <thinhnt@lifull-tech.vn>
2020-02-12 11:35:42 +0700

    Fix code BlogController

--- app/Http/Controllers/User/BlogController.php
+++ app/Http/Controllers/User/BlogController.php
@@ -13,8 +13,7 @@ class BlogController extends Controller
 {
 
     // return information user
-    public function defineUser()
-    {
+    public function defineUser() {
         return Auth::user();
     }
 
@@ -25,13 +24,16 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $blogs = Blog::where('user_id', '=', $this->defineUser()->id)->get();
-        $canDelete = $this->defineUser()->can_delete === Blog::IS_TRUE;
         $canSee = $this->defineUser()->can_see;
-        if ($canSee === Blog::IS_TRUE) {
-            return view('blog/index', ['blogs'=>$blogs, 'canDelete'=>$canDelete]);
-        } else {
+        if ($canSee === Blog::IS_FALSE) {
             return redirect()->back()->withErrors("you can't to enter blog manage");
+        }
+
+        //return view blog index with param $candelete and $blog
+        if ($canSee === Blog::IS_TRUE) {
+            $canDelete = $this->defineUser()->can_delete === Blog::IS_TRUE;
+            $blogs = Blog::where('user_id', '=', $this->defineUser()->id)->get();
+            return view('blog/index', ['blogs'=>$blogs, 'canDelete'=>$canDelete]);
         }
     }
 
@@ -62,51 +64,56 @@ class BlogController extends Controller
     }
 
     /**
-     * Delete one blog.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    * Delete one blog.
+    *
+    * @param  $id
+    * @return \Illuminate\Http\Response
+    */
     public function destroy($id)
     {
-        if ($this->defineUser()->can_delete === Blog::IS_TRUE) {
-            $blog = Blog::find($id);
-            if (isset($blog)) {
-                try {
-                    $blog->delete();
-                } catch (Exception $e) {
-                    return redirect()->back()->withErrors('Delete blog error');
-                }
-            } else {
-                return redirect()->back()->withErrors('Delete blog error');
-            }
+        if ($this->defineUser()->can_delete === Blog::IS_FALSE) {
+            return redirect()->back()->withErrors('You need administrator permission to delete this blog !');
+        }
+
+        // Find and delete
+        $blog = Blog::find($id);
+        if (!isset($blog)) {
+            return redirect()->back()->withErrors('Blog not found !');
+        }
+
+        //delete blog and return back with status
+        try {
+            $blog->delete();
+        } catch (Exception $e) {
+            return redirect()->back()->withErrors('Delete blog error');
         }
 
         return redirect()->back()->with('message', 'Delete blog success !');
     }
 
     /**
-     * Edit view blog.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    * Edit view blog.
+    *
+    * @param  $id
+    * @return \Illuminate\Http\Response
+    */
     public function edit($id)
     {
         $blog = Blog::find($id);
         if (isset($blog)) {
             return view('blog/edit', ['blog' => $blog]);
-        } else {
-            return redirect()->back()->withErrors('Blog not found');
         }
+
+        return redirect()->back()->withErrors('Blog not found !');
     }
 
     /**
-     * Update one blog.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    * Update one blog.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param $id
+    * @return \Illuminate\Http\Response
+    */
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -114,18 +121,19 @@ class BlogController extends Controller
             'content' => 'required',
         ]);
         $blog = Blog::find($id);
-        if (isset($blog)) {
-            try {
-                $blog->title = $request->title;
-                $blog->content = $request->content;
-                $blog->save();
-            } catch (Exception $e) {
-                return redirect()->back()->withErrors('Update blog error');
-            }
-
-            return redirect()->route('blog.index')->with('message', 'Update blog success!');
+        if (!isset($blog)) {
+            return redirect()->back()->withErrors('Blog not found !');
         }
 
-        return redirect()->back()->withErrors('Update blog error!');
+        //update blog
+        try {
+            $blog->title = $request->title;
+            $blog->content = $request->content;
+            $blog->save();
+        } catch (Exception $e) {
+            return redirect()->back()->withErrors('Update blog error');
+        }
+
+        return redirect()->route('blog.index')->with('message', 'Update blog success!');
     }
 }
