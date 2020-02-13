e43e9da3000250f6501ac754ab18c93ded02da9f
Nguyễn Tiến Thịnh <thinhnt@lifull-tech.vn>
2020-02-13 11:32:39 +0700

    Fix route

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::middleware('auth')->group(function () {
 
     //Route User
     Route::namespace('User')->group(function () {
-        Route::get('/', 'BlogController@index')->name('blog.index');
+        Route::get('/blog', 'BlogController@index')->name('blog.index');
         Route::post('/blog/stores', 'BlogController@store')->name('blog.store');
         Route::get('/blog/{id}/delete', 'BlogController@destroy')->name('blog.destroy');
         Route::get('/blog/{id}/edit', 'BlogController@edit')->name('blog.edit');
